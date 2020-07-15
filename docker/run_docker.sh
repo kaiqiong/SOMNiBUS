b2de8197d8cd64854e0287c6d7b44259182e44f1
simonll <simon@mims.ai>
2020-07-15 14:58:16 -0400

    minor

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -2,4 +2,4 @@
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export THISDIR
 export PROJECTROOT=$THISDIR/../
-docker run --rm -e DISABLE_AUTH=true -p 8787:8787 -v $PROJECTROOT:/home/rstudio/somnibus dk-somnibus
+docker run --rm -e DISABLE_AUTH=true -p 8787:8787 -v $PROJECTROOT:/home/rstudio/somnibus dk-somnibus:v1
