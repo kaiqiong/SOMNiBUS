d31afe82f27c3c2ceb01847127762eef2bf5ca83
simonll <simon@mims.ai>
2020-07-17 14:35:59 -0400

    raised flags bitbucket pipes and added add_tex_packages R script

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -2,4 +2,7 @@
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export THISDIR
 export PROJECTROOT=$THISDIR/../
-docker run --rm -e DISABLE_AUTH=true -p 8787:8787 -v $PROJECTROOT:/home/rstudio/somnibus dk-somnibus:v1
+IMAGE="happyregistry.azurecr.io/methods/wes/somnibus"
+VERSION="v1"
+docker pull $IMAGE:$VERSION
+docker run --rm -e DISABLE_AUTH=true -p 8787:8787 -v $PROJECTROOT:/home/rstudio/somnibus $IMAGE:$VERSION
---
+++ scripts/add_tex_packages.R
@@ -0,0 +1,3 @@
+#!/usr/bin/env Rscript
+library(tinytex)
+tinytex::tlmgr_install(c("symbol","ec"))
