2c66d81a7e1d4c5543412b6552c0145659db6198
simonll <simon@mims.ai>
2020-07-20 13:31:03 -0400

    downgrading to r 3.6.3

--- docker/make_docker.sh
+++ docker/make_docker.sh
@@ -9,7 +9,7 @@ echo "PROJECTROOT: " $PROJECTROOT
 echo "GOING TO BUILD $IMAGE:$VERSION"
 docker build $PROJECTROOT --file Dockerfile -t $IMAGE
 echo "$IMAGE BUILDED"
-docker tag $IMAGE happyregistry.azurecr.io/methods/wes/$IMAGE:$VERSION
+docker tag $IMAGE:$VERSION happyregistry.azurecr.io/methods/wes/$IMAGE:$VERSION
 echo "GOING TO PUSH $IMAGE:$VERSION"
 docker push happyregistry.azurecr.io/methods/wes/$IMAGE:$VERSION
 echo "$IMAGE:$VERSION PUSHED"
