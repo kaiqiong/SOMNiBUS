c330644cd925d53077551fd055c211edf6a307ba
simonll <simon@mims.ai>
2020-07-21 10:27:12 -0400

    updated test_Hessian

--- tests/testthat/test_Hessian.R
+++ tests/testthat/test_Hessian.R
@@ -1,6 +1,6 @@
-context("testing Hessian")
+context("testing hessianComp")
 
 test_that("we don't know much about the parameters passed and what is computed here", {
   # this is wrong, we should define a proper test here
-  expect_error(Hessian(1))
+  expect_error(hessianComp(1))
 })
