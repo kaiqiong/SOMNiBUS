75c2c4e3c7a7bca780aa0b23c4159c36fd8a8fd7
kaiqiong <kaiqiong.zhao@mail.mcgill.ca>
2020-08-19 23:03:11 -0400

    modify the argument of function hessianComp such that the size of input data in the test folder is reduced

--- R/binomRegMethModel.R
+++ R/binomRegMethModel.R
@@ -162,15 +162,16 @@ binomRegMethModel <- function(data, n.k, p0=0.003, p1=0.9, Quasi=TRUE, epsilon=1
     #code used to generate
     #/tests/testthat/data/ref_input_hessianComp.RDS
     #input = list(w_ij = w_ij, new.par=out$par, new.lambda=out$lambda,
-    #             X=fitGamOut$data$X, Y=fitGamOut$data$Y, my.design.matrix=my.design.matrix,
-    #             gam.int=out$GamObj, Z=Z, pred.pi=out$pi.ij, p0=p0, p1=p1,
-    #             disp_est=phi_fletcher, RanEff=RanEff, N=lengthUniqueDataID)
+    #X=fitGamOut$data$X, Y=fitGamOut$data$Y, my.design.matrix=my.design.matrix,
+    #gam_smoothMat=out$GamObj$smooth, Z=Z, pred.pi=out$pi.ij, p0=p0, p1=p1,
+    #disp_est=phi_fletcher, RanEff=RanEff, N=lengthUniqueDataID)
     #path_ref_input_hessianComp <-
     #    paste(paste(getwd(), '/tests/testthat/data/', sep=''),
     #          'ref_input_hessianComp.RDS', sep='')
     # saveRDS(input, path_ref_input_hessianComp)
     H <- hessianComp(w_ij=w_ij, new.par=out$par,
-        new.lambda=out$lambda,  X=fitGamOut$data$X, Y=fitGamOut$data$Y, my.design.matrix=my.design.matrix, gam.int=out$GamObj, Z=Z, pred.pi=out$pi.ij, p0=p0, p1=p1,
+        new.lambda=out$lambda,  X=fitGamOut$data$X, Y=fitGamOut$data$Y, my.design.matrix=my.design.matrix,
+        gam_smoothMat = out$GamObj$smooth, Z=Z, pred.pi=out$pi.ij, p0=p0, p1=p1,
         disp_est=phi_fletcher, RanEff=RanEff, N=lengthUniqueDataID)
 
     var.cov.alpha <- solve(-H)
@@ -321,9 +322,7 @@ binomRegMethModelSummary <- function(GamObj, var.cov.alpha, new.par, edf.out, ed
     for (i in seq_len(m)) {
         start <- GamObj$smooth[[i]]$first.para
         stop <- GamObj$smooth[[i]]$last.para
-
         V <- var.cov.alpha[start:stop, start:stop, drop=FALSE]  ## Bayesian
-
         p <- new.par[start:stop]  ## params for smooth
         edfi <- sum(edf.out[start:stop])  ## edf for this smooth
         ## extract alternative edf estimate for this smooth, if possible...
