a6523e867adeb4461afc2f8a9d4322677c8e3d44
kaiqiong <kaiqiong.zhao@mail.mcgill.ca>
2020-08-19 20:44:25 -0400

    Add the test for hessianComp; update the ref_results.RDS for the new functions with 17 outputs

--- tests/testthat/test_hessianComp.R
+++ tests/testthat/test_hessianComp.R
@@ -1,6 +1,23 @@
 context("testing hessianComp")
 
-test_that("we don't know much about the parameters passed and what is computed here", {
-  # this is wrong, we should define a proper test here
-  expect_error(hessianComp(1))
+path_ref_results <- paste(paste(getwd(), "/data/", sep = ""), "ref_results.RDS",
+                                                sep = ""
+)
+ref <- readRDS(path_ref_results)
+
+path_ref_input_hessianComp <- paste(paste(getwd(), "/data/", sep = ""), "ref_input_hessianComp.RDS",
+                                                sep = ""
+)
+ref_input_hessianComp <- readRDS(path_ref_input_hessianComp)
+
+H <- hessianComp(w_ij=w_ij, new.par=out$par,
+                 new.lambda=out$lambda,  X=fitGamOut$data$X, Y=fitGamOut$data$Y, my.design.matrix=my.design.matrix, gam.int=out$GamObj, Z=Z, pred.pi=out$pi.ij, p0=p0, p1=p1,
+                 disp_est=phi_fletcher, RanEff=RanEff, N=lengthUniqueDataID)
+
+test_that("hessianComp calculates the correct Hessian matrix", {
+    expect_equal(nrow(H), length(out$par))
+    expect_equal(ncol(H), length(out$par))
+    expect_true(isTRUE(all.equal(solve(-H), ref$cov1)))
+    #expect_true(all(solve(-H) - ref$cov1 <0.000001))
 })
+
