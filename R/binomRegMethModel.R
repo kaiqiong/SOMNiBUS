b20637b6cb839ad273476d1fc5e399bb225c0c84
kaiqiong <kaiqiong.zhao@mail.mcgill.ca>
2020-08-19 21:15:23 -0400

    Add the test for function binomRegMethModelSummary

--- R/binomRegMethModel.R
+++ R/binomRegMethModel.R
@@ -168,7 +168,7 @@ binomRegMethModel <- function(data, n.k, p0=0.003, p1=0.9, Quasi=TRUE, epsilon=1
     #path_ref_input_hessianComp <-
     #    paste(paste(getwd(), '/tests/testthat/data/', sep=''),
     #          'ref_input_hessianComp.RDS', sep='')
-    #saveRDS(input,       path_ref_input_hessianComp)
+    # saveRDS(input, path_ref_input_hessianComp)
     H <- hessianComp(w_ij=w_ij, new.par=out$par,
         new.lambda=out$lambda,  X=fitGamOut$data$X, Y=fitGamOut$data$Y, my.design.matrix=my.design.matrix, gam.int=out$GamObj, Z=Z, pred.pi=out$pi.ij, p0=p0, p1=p1,
         disp_est=phi_fletcher, RanEff=RanEff, N=lengthUniqueDataID)
--- tests/testthat/test_binomRegMethModelSummary.R
+++ tests/testthat/test_binomRegMethModelSummary.R
@@ -1,6 +1,28 @@
 context("testing binomRegMethModelSummary")
 
-test_that("we don't know much about the parameters passed and what is computed here", {
-  # this is wrong, we should define a proper test here
-  expect_error(binomRegMethModelSummary(1))
+path_ref_results <- paste(paste(getwd(), "/data/", sep = ""), "ref_results.RDS",
+                          sep = ""
+)
+ref <- readRDS(path_ref_results)
+
+path_ref_input_binomRegMethModelSummary <- paste(paste(getwd(), "/data/", sep = ""), "ref_input_binomRegMethModelSummary.RDS",
+                                    sep = ""
+)
+ref_input_binomRegMethModelSummary <- readRDS(path_ref_input_binomRegMethModelSummary)
+
+
+
+
+s.table <- binomRegMethModelSummary( ref_input_binomRegMethModelSummary$GamObj, ref_input_binomRegMethModelSummary$var.cov.alpha,
+                                     ref_input_binomRegMethModelSummary$new.par ,ref_input_binomRegMethModelSummary$edf.out,
+                                     ref_input_binomRegMethModelSummary$edf1.out, ref_input_binomRegMethModelSummary$X_d,
+                                     ref_input_binomRegMethModelSummary$resi_df, ref_input_binomRegMethModelSummary$Quasi,
+                                     ref_input_binomRegMethModelSummary$scale, ref_input_binomRegMethModelSummary$RanEff,
+                                     ref_input_binomRegMethModelSummary$Z)
+
+
+test_that("binomRegMethModelSummary provides the correct regional p-value summary", {
+    expect_equal(nrow(s.table), 4)
+    expect_equal(ncol(s.table), 3)
+    expect_true(isTRUE(all.equal(s.table, ref$reg.out)))
 })
